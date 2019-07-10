1830a63628464a25d4ad1741bd867daea33cc566
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 01:05:04 +0200

    Add files via upload

---
+++ Phish.py
@@ -0,0 +1,20 @@
+def phish () :
+    import os
+    os.system("clear && toilet -f big 'Phishing Tools' | lolcat ")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/samyoyo/weeman")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/UndeadSec/SocialFish.git")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/thelinuxchoice/blackeye")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/thelinuxchoice/shellphish")
+    import os
+    os.system("cd /$HOME && git clone https://github.com/Cabdulahi/pish")
+    import os
+    os.system("cd /$HOME && git clone -b Termux-Support-Branch https://github.com/DarkSecDevelopers/HiddenEye.git")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/UndeadSec/EvilURL.git")
+    import os 
+    os.system("clear && toilet -f big 'DONE...! ENJOY HACKER ' | lolcat")
+phish()
---
+++ requirements.sh
@@ -0,0 +1,5 @@
+pkg install php openssh python curl openssl-tool nmap ruby perl toilet figlet fish cowsay tree -y && gem install lolcat && pkg upgrade -y && pkg install net-tools hydra -y
+
+clear
+
+toilet -f big ' DONE..!' | lolcat 
